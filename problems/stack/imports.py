77b917a23e7a30913a40c59dc7fb29f1f945df68
jfmonsa <juanmonsalve@protonmail.com>
2023-11-03 12:47:28 -0500

    import.py in packages to avoid 'redundant' imports in every .py file

---
+++ problems/stack/imports.py
@@ -0,0 +1,7 @@
+"""
+Avoid importing the same modules in all files of this directory,
+import this file in the other .py files which needs this modules
+"""
+
+from implement_ds import Stack
+import re, copy
