144b17d5924fb8af4161c740fdfdbd9eb221196e
jfmonsa <juanmonsalve@protonmail.com>
2024-06-24 23:09:56 -0500

    Adding updatedNode and some logic fixes

--- data_structures_implementation/linkedList.py
+++ data_structures_implementation/linkedList.py
@@ -16,33 +16,28 @@ class LinkedList:
 
     def size(self):
         count = 0
-        if not self.head:
-            return 0
-        else:
-            current_node = self.head
-            while current_node:
-                count += 1
-                current_node = current_node.next
-            return count
+        current_node = self.head
+        while current_node:
+            count += 1
+            current_node = current_node.next
+        return count
 
     def search(self, elm):
-        if self.size() > 0:
-            count = 0
-            current_node = self.head
-            while current_node:
-                if current_node.data == elm:
-                    # returnning the index of the element
-                    return count
-                count += 1
-            return None
-        else:
-            raise Exception("No elements in the Linked List")
+        count = 0
+        current_node = self.head
+        while current_node:
+            if current_node.data == elm:
+                # returning the index of the element
+                return count
+            current_node = current_node.next
+            count += 1
+        return None
 
     # Insertion
     def insertAtStart(self, data):
         new_node = LinkedListNode(data)
         if self.head == None:
-            self.head == new_node
+            self.head = new_node
         else:
             new_node.next = self.head
             self.head = new_node
@@ -78,7 +73,19 @@ class LinkedList:
 
     # Update
     def updateNode(self, data, pos):
-        pass
+        if self.head is None:
+            raise Exception("Empty LinkedList")
+
+        count = 0
+        current_node = self.head
+        while current_node and count != pos:
+            current_node = current_node.next
+            count += 1
+
+        if current_node:
+            current_node.data = data
+        else:
+            raise Exception("Out of index error")
 
     # Deletion
     def delAtStart(self):
@@ -90,6 +97,11 @@ class LinkedList:
     def delAtEnd(self):
         if self.head is None:
             raise Exception("UnderflowError: Linked list is already empty")
+
+        if self.head.next is None:
+            self.head = None
+            return
+
         current_node = self.head
         while current_node.next.next:
             current_node = current_node.next
@@ -98,20 +110,47 @@ class LinkedList:
     def delAtPos(self, pos):
         if self.head is None:
             raise Exception("UnderflowError: Linked list is already empty")
-        else:
-            count = 0
-            current_node = self.head
-            if pos == 0:
-                self.delAtStart()
-            else:
-                while current_node and count + 1 != pos:
-                    current_node.next = current_node.next.next
-                # Change pointers here
-                if current_node:
-                    current_node.next = current_node.next.next
-                else:
-                    raise Exception("OutOfIndex: error index not present")
+
+        if pos == 0:
+            self.delAtStart()
+            return
+
+        count = 0
+        current_node = self.head
+        while current_node and count < pos - 1:
+            current_node = current_node.next
+            count += 1
+
+        if current_node is None or current_node.next is None:
+            raise Exception("OutOfIndex: error index not present")
+
+        current_node.next = current_node.next.next
+
+    # method to print LinkedList
+    def __str__(self):
+        current_node = self.head
+        str_aux = ""
+        while current_node:
+            str_aux += f"[{current_node.data}] -> "
+            current_node = current_node.next
+        str_aux += "null"
+        return str_aux
 
 
 if "__main__" == __name__:
-    pass
+
+    def main():
+        lst1 = LinkedList()
+        lst1.insertAtStart(3)
+        lst1.insertAtStart(2)
+        lst1.insertAtPos(10, 2)
+        lst1.insertAtEnd(4)
+        lst1.insertAtEnd(5)
+        lst1.insertAtEnd(25)
+        lst1.delAtEnd()
+        lst1.delAtStart()
+        lst1.updateNode(32, 0)
+        lst1.delAtPos(3)
+        print(lst1)
+
+    main()
